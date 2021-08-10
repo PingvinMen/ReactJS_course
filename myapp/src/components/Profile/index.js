7dfafda7cc0ddcf22c25ce90ac48bf375f03de67
PingvinMen <55557719+PingvinMen@users.noreply.github.com>
2021-08-10 22:41:46 +0300

    Задание 5

--- myapp/src/components/Profile/index.js
+++ myapp/src/components/Profile/index.js
@@ -1,5 +1,27 @@
 import React from "react";
+//import { store } from "../../store";
+import { PROFILE_TOGGLE_SHOW } from "../../store/actionTypes";
+import { useSelector, useDispatch } from "react-redux";
 
 export const Profile = () => {
-    return <div>Профиль</div>;
+    const profileState = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    const toggleShow = () => {
+        dispatch({
+            type: PROFILE_TOGGLE_SHOW,
+        });
+      };
+
+    console.log(profileState);
+
+    return (
+        <>
+            <div>Профиль</div>
+
+            <input type="checkbox"  onClick={toggleShow}/>
+        </>
+    );
 };
+
+/*Я знаю что это не правильно, я постараюсь переделать до проверки(оно только ТИПА работает)*/
